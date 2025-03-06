073763cbeeba7c71a92fe0c0cf5a694e327125db
Lhimor-Entino <lhimortinoen007@gmail.com>
2025-03-06 17:19:51 +0800

    UPDATE 11

--- src/app/(user-page)/(tab-group-layout)/products/page.tsx
+++ src/app/(user-page)/(tab-group-layout)/products/page.tsx
@@ -2,7 +2,7 @@
 import { ProductCard } from "@/components/ProductCard";
 import { product_data } from "@/data/product";
 
-const ProductPage = () => {
+export default function ProductPage() {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 grid-flow-row gap-2  place-items-center">
       {product_data.length > 0
@@ -18,4 +18,3 @@ const ProductPage = () => {
   );
 };
 
-export default ProductPage;

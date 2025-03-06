905e61f6421206ff07c43e168111188d27dc2677
Lhimor-Entino <lhimortinoen007@gmail.com>
2025-03-06 16:54:52 +0800

    UPDATE 7

--- src/app/(user-page)/(tab-group-layout)/products/[type]/page.tsx
+++ src/app/(user-page)/(tab-group-layout)/products/[type]/page.tsx
@@ -1,46 +1,46 @@
-import type { Metadata, ResolvingMetadata } from "next";
+import type { Metadata } from "next";
 import { notFound } from "next/navigation";
 import { product_data } from "@/data/product";
 import { ProductCard } from "@/components/ProductCard";
-import ProductSidebarLoadingSkeleton from "@/components/ProductSidebarLoadingSkeleton";
 import ProductsSidebar from "@/components/ProductsSidebar";
 
 // DYNAMIC METADATA
 export async function generateMetadata({
   params,
 }: {
-  params: { type: string };
+  params: { type?: string };
 }): Promise<Metadata> {
-  // read route params
-  const type = params.type;
-
+  const type = params.type?.toUpperCase() || "UNKNOWN";
   return {
-    title: `AgriHive - Products (${type.toUpperCase()})`,
+    title: `AgriHive - Products (${type})`,
     description: "Products",
   };
 }
-const ProducTypePage = ({ params }: { params: { type: string } }) => {
-  if (params.type !== "seafoods" && params.type !== "poultry") {
+
+const ProductTypePage = ({ params }: { params: { type?: string } }) => {
+  if (!params.type || (params.type !== "seafoods" && params.type !== "poultry")) {
     return notFound();
   }
 
   return (
     <div className="flex">
-      <div className="w-56  mr-2">
+      <div className="w-56 mr-2">
         <ProductsSidebar />
       </div>
-      <div className=" w-full mt-2">
+      <div className="w-full mt-2">
         <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 grid-flow-row gap-2 border-2 border-red-50 place-items-center">
-          {product_data?.map((pd, index) =>
-            pd?.productType.toLowerCase() === params.type ? (
-              <ProductCard
-                info={pd}
-                link={pd.product_name.toLocaleLowerCase() + "/" + pd?.id}
-                key={index}
-              />
-            ) : (
-              ""
+          {product_data?.length > 0 ? (
+            product_data.map((pd, index) =>
+              pd?.productType.toLowerCase() === params.type ? (
+                <ProductCard
+                  info={pd}
+                  link={pd.product_name.toLowerCase() + "/" + pd?.id}
+                  key={index}
+                />
+              ) : null
             )
+          ) : (
+            <p>No products found.</p>
           )}
         </div>
       </div>
@@ -48,4 +48,4 @@ const ProducTypePage = ({ params }: { params: { type: string } }) => {
   );
 };
 
-export default ProducTypePage;
+export default ProductTypePage;

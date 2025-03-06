e27e1080d0da5e498dd57e4b3c3c5db65d4ca451
Lhimor-Entino <lhimortinoen007@gmail.com>
2025-03-06 17:07:03 +0800

    UPDATE 8

--- src/app/(user-page)/(tab-group-layout)/products/page.tsx
+++ src/app/(user-page)/(tab-group-layout)/products/page.tsx
@@ -7,18 +7,12 @@ const ProductPage = () => {
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 grid-flow-row gap-2  place-items-center">
       {product_data.length > 0
         ? product_data?.map((pd, index) => (
-            <ProductCard
-              info={pd}
-              key={index}
-              link={
-                pd.productType.toLocaleLowerCase() +
-                "/" +
-                pd.product_name.toLocaleLowerCase() +
-                "/" +
-                pd?.id
-              }
-            />
-          ))
+          <ProductCard
+            info={pd}
+            key={index}
+            link={`/products/${pd.productType.toLowerCase()}/${pd.product_name.toLowerCase()}/${pd.id}`}
+          />
+        ))
         : ""}
     </div>
   );
